ac538b7e023691d8c54890ccf5cc2ca5b3feadab
Killian <killian.delamarre-ruer@students.dominican.edu>
2026-02-11 19:12:33 -0800

    updated app.js for new proj, need to do pages now

--- App.js
+++ App.js
@@ -2,38 +2,51 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
-import DogsStack from './DogsStack';
-import CatsStack from './CatsStack';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import AllRecipesStack from './AllRecipesStack';
+import FavoritesStack from './FavoritesStack';
+import ShoppingListStack from './ShoppingListStack';
 
 const Tab = createBottomTabNavigator();
 
+function TabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          const icons = {
+            'All Recipes': focused ? 'restaurant' : 'restaurant-outline',
+            'Favorites': focused ? 'heart' : 'heart-outline',
+            'Shopping List': focused ? 'cart' : 'cart-outline',
+          };
+
+          return (
+            <Ionicons
+              name={icons[route.name] ?? 'help-circle-outline'}
+              size={size}
+              color={color}
+            />
+          );
+        },
+        tabBarActiveTintColor: '#111',
+        tabBarInactiveTintColor: '#999',
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen name="All Recipes" component={AllRecipesStack} />
+      <Tab.Screen name="Favorites" component={FavoritesStack} />
+      <Tab.Screen name="Shopping List" component={ShoppingListStack} />
+    </Tab.Navigator>
+  );
+}
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            const icons = {
-              Dogs: focused ? 'paw' : 'paw-outline',
-              Cats: focused ? 'paw' : 'paw-outline',
-            };
-
-            return (
-              <Ionicons
-                name={icons[route.name] ?? 'help-circle-outline'}
-                size={size}
-                color={color}
-              />
-            );
-          },
-          tabBarActiveTintColor: '#111',
-          tabBarInactiveTintColor: '#999',
-          headerShown: false,
-        })}
-      >
-        <Tab.Screen name="Dogs" component={DogsStack} />
-        <Tab.Screen name="Cats" component={CatsStack} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 }
