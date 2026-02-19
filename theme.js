8511cc72c26bd14998401a87e1294ed339bda2fb
Killian <killian.delamarre-ruer@students.dominican.edu>
2026-02-18 20:40:38 -0800

    added theme file

---
+++ theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+    colors: {
+      primary: '#002395',
+      danger: '#ED2939',
+      background: '#fff',
+      surface: '#f9f9f9',
+      border: '#eee',
+      textPrimary: '#333',
+      textSecondary: '#666',
+    },
+    spacing: { sm: 8, md: 16, lg: 24 },
+    radius: { sm: 8, md: 14, lg: 20 },
+    typography: { body: 16, label: 14, small: 12, heading: 20, title: 18 },
+  };
